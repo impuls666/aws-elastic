29a2f280341d2a396402fd8bf9786d72dbf4be8d
Lubomir Marcinko <lubomir.marcinko@gmail.com>
2023-11-23 07:55:28 +0100

    a

--- main.tf
+++ main.tf
@@ -52,26 +52,84 @@ resource "aws_s3_bucket" "artifacts" {
   bucket = "codepipeline-artifacts-bucket2023-1"
 }
 
+
+#codebuild
+
+# CodeBuild Project
+
 resource "aws_codebuild_project" "example" {
   name = "codebuild-project"
 
   source {
-    type = "CODEPIPELINE"   
+    type = "CODEPIPELINE"
   }
 
-    environment {
+  environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
   }
 
-    artifacts {
+  artifacts {
     type = "CODEPIPELINE"
   }
 
   service_role = aws_iam_role.example.arn
 }
+
+# CodePipeline
+
+resource "aws_codestarconnections_connection" "example" {
+  name          = "example-connection"
+  provider_type = "Bitbucket"
+}
+
+resource "aws_codepipeline" "example" {
+  name     = "tf-test-pipeline"
+  
+  role_arn = aws_iam_role.example.arn
+
+artifact_store {
+    location = aws_s3_bucket.artifacts.bucket
+    type     = "S3"
+  }
+  
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.example.arn
+        FullRepositoryId = "impuls666/react-btc"
+        BranchName       = "main"
+      }
+    }
+  }
+    stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+        ProjectName = "test"
+      }
+    }
+  }
+}
+
+
+
 
 #elastic
 resource "aws_elastic_beanstalk_application" "example" {
